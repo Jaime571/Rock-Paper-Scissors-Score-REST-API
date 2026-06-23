45049ac4986f4a7ba6384d7116297bd033763dab
Jaime García Mayorga <lockdown97140@gmail.com>
2026-06-22 23:29:03 -0600

    First commit

---
+++ src/test/java/com/mayorga/rest/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayorga.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
